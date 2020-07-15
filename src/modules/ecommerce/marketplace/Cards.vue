b617a73dca7abc2fb9fa7db96e8f2ff639929af8
HTundra <kentbacatan@gmail.com>
2020-07-15 22:23:12 +0800

    ecommercecards

--- src/modules/ecommerce/marketplace/Cards.vue
+++ src/modules/ecommerce/marketplace/Cards.vue
@@ -1,73 +1,112 @@
 <template>
-  
 <div class="empty">
-    <div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">Init pa ang lumpia dong, naa sa'y utan diha if ganahan ka</p>
-  <hr class="my-4">
-  <div class="container">
-		<div class="row">
-			<div class="col-sm">
-				<div class="card" style="width: 18rem;">
-					<!-- <img src="..." class="card-img-top" alt="..."> -->
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
+	<div class="jumbotron" id="jumboID">
+		<div class="position-absolute w-100">
+			<!-- <img class="img-fluid" src="https://www.arborday.org/images/hero/medium/hero-aerial-forest-evergreen-trees.jpg"> -->
+		</div>
+		<h1 class="display-4 text-white">Hello, world!</h1>
+		<p class="lead text-white">Init pa ang lumpia dong, naa sa'y utan diha if ganahan ka</p>
+		<hr class="my-4">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm mb-4">
+						<div class="card" style="width: 18rem;">
+							<!-- <img src="..." class="card-img-top" alt="..."> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm mb-4">
+						<div class="card" style="width: 18rem;">
+							<!-- <img src="..." class="card-img-top" alt="..."> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm mb-4">
+						<div class="card" style="width: 18rem;">
+							<!-- <img src="..." class="card-img-top" alt="..."> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="col-sm">
-				<div class="card" style="width: 18rem;">
-					<!-- <img src="..." class="card-img-top" alt="..."> -->
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="row">
+					<div class="col-sm mb-4">
+						<div class="card" style="width: 18rem;">
+							<!-- <img src="..." class="card-img-top" alt="..."> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm mb-4">
+						<div class="card" style="width: 18rem;">
+							<!-- <img src="..." class="card-img-top" alt="..."> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm mb-4">
+						<div class="card" style="width: 18rem;">
+							<!-- <img src="..." class="card-img-top" alt="..."> -->
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
 					</div>
 				</div>
+                
 			</div>
-			<div class="col-sm">
-				<div class="card" style="width: 18rem;">
-					<!-- <img src="..." class="card-img-top" alt="..."> -->
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
-					</div>
-				</div>
-			</div>
+			<!-- <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> -->
 		</div>
 	</div>
-  <!-- <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> -->
-</div>
-	
-</div>
 </template>
 <style>
-  .empty{
-    width: 100%;
-    margin-top: 25px;
-    margin-bottom: 25px;
-    float: left;
-    min-height: 450px;
-    overflow-y: hidden;
-    text-align: center;
-    border: solid 1px #ddd;
-  }
-
-  .empty i{
-    font-size: 100px;
-    padding-top: 150px;
-  }
-  .empty span{
-    width: 100%;
-    float: left;
-  }
-
-  .empty .description{
-    font-size: 24px;
-  }
+    #banner{
+       z-index: -1;
+       top: 0;
+       left: 0;
+       background-color:orange;
+    }
+    #jumboID{
+        background:url(https://www.arborday.org/images/hero/medium/hero-aerial-forest-evergreen-trees.jpg) top/100% no-repeat;
+    }
+    .empty{
+      width: 100%;
+      margin-top: 25px;
+      margin-bottom: 25px;
+      float: left;
+      min-height: 450px;
+      overflow-y: hidden;
+      text-align: center;
+      border: solid 1px #ddd;
+    }   .empty i{
+      font-size: 100px;
+      padding-top: 150px;
+    }
+    .empty span{
+      width: 100%;
+      float: left;
+    }   .empty .description{
+      font-size: 24px;
+    }
 </style>
 <script>
 import ROUTER from 'src/router'
