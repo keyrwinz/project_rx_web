f9e1f358f6e388a3d1ee788f9e422d68303812f4
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2020-09-08 17:12:40 +0800

    Categories on Landing Page

--- src/modules/home/settings.js
+++ src/modules/home/settings.js
@@ -16,46 +16,50 @@ export default {
     background: '#fff'
   },
   categories: [{
-    title: 'Food & Beverage',
+    title: 'Foods & Beverages',
     description: 'Reward yourself with delicious foods.',
-    subDescription: 'Reward yourself with delicious foods. Download our app and order your food today!',
-    icon: 'fas fa-envelope',
-    image: require('src/assets/img/banner.png')
+    subDescription: 'Satisfy your cravings for your favorite foods and drinks anywhere you are. Download our app and order today!',
+    partDescription: 'Want to optimize your food and drink business? Make it happen with Runway Express!',
+    icon: 'fas fa-glass-cheers',
+    image: require('src/assets/img/foodrink.jpg')
   }, {
     title: 'Errands',
     description: 'Just make it easy, click and wait for your documents.',
-    subDescription: 'Sometimes the bests moments are the ones you did not plan at all. Download our app and order your food today!',
-    icon: 'fas fa-store',
-    image: require('src/assets/img/banner.png')
+    subDescription: 'Want hassle-free? Do not worry, relax and let your errands run with us. Download our app today!',
+    icon: 'fas fa-running',
+    image: require('src/assets/img/errands.jpg')
   }, {
     title: 'Groceries',
-    description: 'Reward yourself with delicious foods.',
-    subDescription: 'Sometimes the bests moments are the ones you did not plan at all. Download our app and order your food today!',
-    icon: 'fas fa-store',
-    image: require('src/assets/img/banner.png')
+    description: 'Groceries deliver at your doorstep.',
+    subDescription: 'Run out-of-stock? Just wait and do anything at home until your grocery knocks at your door. Download our app and order today!',
+    icon: 'fas fa-shopping-basket',
+    image: require('src/assets/img/grocery.jpg')
   }, {
     title: 'Plants & Seeds',
-    description: 'Reward yourself with delicious foods.',
-    subDescription: 'Sometimes the bests moments are the ones you did not plan at all. Download our app and order your food today!',
-    icon: 'fas fa-store',
-    image: require('src/assets/img/banner.png')
+    description: 'Grow your love and passion in nature.',
+    subDescription: 'Love gardening? Make it happen, no more long travels. Get a wider selection. Download our app and order seeds or plants today!',
+    partDescription: 'Do you sell plants and seeds? Make use of Runway Express!',
+    icon: 'fas fa-seedling',
+    image: require('src/assets/img/plantseed.jpg')
   }, {
     title: 'Hardware Supplies',
-    description: 'Reward yourself with delicious foods.',
-    subDescription: 'Sometimes the bests moments are the ones you did not plan at all. Download our app and order your food today!',
-    icon: 'fas fa-store',
-    image: require('src/assets/img/banner.png')
+    description: 'Find and select more supplies at one-stop source.',
+    subDescription: 'Needed hardware supplies, buying hard no more. Download our app and order your hardware supplies today! ',
+    partDescription: 'Own a hardware business? Reach your customers with Runway Express!',
+    icon: 'fas fa-store-alt',
+    image: require('src/assets/img/hardware.jpg')
   }, {
     title: 'Online Shops',
-    description: 'Reward yourself with delicious foods.',
-    subDescription: 'Sometimes the bests moments are the ones you did not plan at all. Download our app and order your food today!',
-    icon: 'fas fa-store',
-    image: require('src/assets/img/banner.png')
+    description: 'Get more! Slow shopping no more.',
+    subDescription: 'Life is hard enough already. Love shopping? Let us help you with making it easier and simpler for you. Download our app and shop for what you want today!',
+    icon: 'fa fa-shopping-cart',
+    image: require('src/assets/img/shoponline.jpg')
   }, {
     title: 'Pharmacy & Cosmetics',
-    description: 'Reward yourself with delicious foods.',
-    subDescription: 'Sometimes the bests moments are the ones you did not plan at all. Download our app and order your food today!',
-    icon: 'fas fa-store',
-    image: require('src/assets/img/banner.png')
+    description: 'Best way to shop for health & beauty.',
+    subDescription: 'Keep safe. Stay healthy and beautiful. Let us help you save time and money. Medications and more deliver at your door. Download our app today!',
+    partDescription: 'Own a Pharmacy or Cosmetics business? Reach out and let your patients feel your care with Runway Express!',
+    icon: 'fas fa-clinic-medical',
+    image: require('src/assets/img/pharma.jpg')
   }]
 }
